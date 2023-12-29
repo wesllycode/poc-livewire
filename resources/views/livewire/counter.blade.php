b739924c4b9700e6fa4441a946e81cad23216d8e
wesllycode <wesllycode07@gmail.com>
2023-12-28 22:50:10 -0300

    feat(exampleDocs): primeiro exemplo de acordo com doc concluida

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,7 +1,7 @@
 <div>
     <h1>{{ $count }}</h1>
 
-    <button wire:click="increment"></button>
+    <button wire:click="increment">Acrescentar</button>
 
-    <button wire:click="decrement"></button>
+    <button wire:click="decrement">Decrementar</button>
 </div>
